05321adeb5df83e38f42de2cedac8da7ac07b2b2
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-06-17 19:03:28 +0300

    Updated trial.py

--- trial.py
+++ trial.py
@@ -1,29 +1,46 @@
 # Python program For Birthday Reminder Application
 import time
+from datetime import datetime
 import csv
 import os
 import pandas as pd
 import csv
-rows = []
-with open("birthday_data.csv", "r") as file:
-    csvreader = csv.reader(file)
-    header = next(csvreader)
-    for row in csvreader:
-        rows.append(row)
-print(header)
-print(rows)
+# rows = []
+# with open("birthday_data.csv", "r") as file:
+#     csvreader = csv.reader(file)
+#     header = next(csvreader)
+#     for row in csvreader:
+#         rows.append(row)
+# print(header)
+# print(rows)
 
-# file = open('/Users/aurimasnausedas/Documents/Python/BirthdayReminderApp/birthday_data.csv')
-# print(type(file))
-# csvreader = csv.reader(file)
+file = open('/Users/aurimasnausedas/Documents/Python/BirthdayReminderApp/birthday_data.csv')
+csvreader = csv.reader(file)
 # header = []
 # header = next(csvreader)
 # print(header)
-# rows = []
-# for row in csvreader:
-#     rows.append(row)
-# print(rows)
-# file.close()
+rows = []
+for row in csvreader:
+    rows.append(row)
+date_str = '2000-06-17'
+for item in rows:
+    # print(item[2])
+    # if item[2] != '':
+    try:
+        # print(datetime.strptime(item[2], "%Y-%m-%d").strftime('%m-%d'))
+        if datetime.strptime(item[2], '%Y-%m-%d').strftime('%m-%d') == time.strftime('%m-%d'):
+            print(True)
+        else:
+            print(False)
+    except Exception as error:
+        print(item[2], error)
+
+print(datetime.strptime(date_str, '%Y-%m-%d' ).strftime('%m-%d')) # formatting to month-day
+
+# date_object = datetime.strptime(date_str, '%Y-%m-%d').strftime('%m-%d')
+# print(type(date_object))
+# print(date_object)  # printed in default formatting
+file.close()
 
 # col_list = ['first_name',  'last_name', 'clean_birth_date']
 # df = pd.read_csv("/Users/aurimasnausedas/Documents/Python/birthdayApp/Clean_Player_Birthdays.csv", usecols=col_list)
@@ -34,28 +51,28 @@ print(rows)
 # # print(df.loc[2]["clean_birth_date"])
 # print(time.strftime(('%y%m%d')))
 # birthdayFile = 'clean-player-birthda-advanced-visualization-sharing-in-data-world-QueryResult.csv'
-
-# def checkTodaysBirthdays():
-#     file_df = pd.read_csv("/Users/aurimasnausedas/Documents/Python/birthdayApp/clean-player-birthda-advanced-visualization-sharing-in-data-world-QueryResult.csv")
-#     # fileName = open(file_df, 'r')
+def checkTodaysBirthdays(rows):
 #     fileName = open(file_df, newline='', encoding='utf-8')
 #     print(fileName)
-    # if type(spreadsheet) == pd.core.frame.DataFrame:
-
-    # today = time.strftime('%m%d')
-    # flag = 0
-    # for line in fileName:
-    #     if today in line:
-    #        line = line.split(' ')
-    #        flag = 1
-    #        # line[1] contains Name and line[2] contains Surname
-    #        os.system('notify-send "Birthdays Today: ' + line[1]
-    #                  + ' ' + line[2] + '"')
-    # if flag == 0:
-    #     os.system('notify-send "No Birthdays Today!"')
-
-# if __name__ == '__main__':
-#  checkTodaysBirthdays()
+#     if type(spreadsheet) == pd.core.frame.DataFrame:
+#
+    today = time.strftime('%m-%d')
+    flag = 0
+    for line in rows:
+        if today in line:
+            print(True)
+        # else:
+        #     print('Come on')
+#            line = line.split(' ')
+#            flag = 1
+#            # line[1] contains Name and line[2] contains Surname
+#            os.system('notify-send "Birthdays Today: ' + line[1]
+#                      + ' ' + line[2] + '"')
+#     if flag == 0:
+#         os.system('notify-send "No Birthdays Today!"')
+#
+if __name__ == '__main__':
+ checkTodaysBirthdays(rows)
 
 # def checkBirthdays():
 #     myBrthdayFile = '/Users/aurimasnausedas/Documents/Python/birthdayApp/birthdays.csv'
