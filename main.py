f02fa9a5afc030f2536f2fc20afa880f228c2062
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-06-17 19:49:57 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
+# Python program For Birthday Reminder Application
+import time
+from datetime import datetime
+import os
+import csv
+
 def checkTodaysBirthdays():
     fileName = open('/Users/aurimasnausedas/Documents/Python/BirthdayReminderApp/birthday_data.csv', 'r')
+
     csvreader = csv.reader(fileName)
     today = time.strftime('%m-%d')
+
     flag = 0
+
     rows = []
-    today_birthdays = []
+    todays_birthdays = []
+
     for row in csvreader:
         rows.append(row)
+
     for item in rows:
+
         try:
             if datetime.strptime(item[2], '%Y-%m-%d').strftime('%m-%d') == today:
                 print(f'{item[0]} {item[1]} has birthday today')
-                today_birthdays.append(item)
+                todays_birthdays.append(item)
                 # print(item)
                 flag = 1
             elif flag == 0:
@@ -21,7 +33,9 @@ def checkTodaysBirthdays():
         except Exception as error:
             print(item[2], error)
 
-    return today_birthdays
+    print(todays_birthdays)
+    return todays_birthdays
+
 
 if __name__ == '__main__':
  checkTodaysBirthdays()
